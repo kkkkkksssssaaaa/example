923d544db247e3a00165bceb90f031d7bba69382
s13110233982692316 <kofc312@springsoft.io>
2021-08-21 21:10:20 +0900

    코드 예제 모듈 생성

---
+++ study/code-example/src/main/java/dev/kkkkkksssssaaaa/codeexample/CodeExampleApplication.java
@@ -0,0 +1,13 @@
+package dev.kkkkkksssssaaaa.codeexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeExampleApplication {
+
+    public static void main (String[] args) {
+        SpringApplication.run(CodeExampleApplication.class, args);
+    }
+
+}
---
+++ study/code-example/src/test/java/dev/kkkkkksssssaaaa/codeexample/CodeExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.kkkkkksssssaaaa.codeexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeExampleApplicationTests {
+
+    @Test
+    void contextLoads () {
+    }
+
+}
