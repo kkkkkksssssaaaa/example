88171797868123f8e547cfca4f472106e8688a7d
kkkkkksssssaaaa <kofc312@naver.com>
2022-04-18 21:44:14 +0900

    feat: #1-1 add theater model

---
+++ study/book-object/sell-ticket/src/main/java/Theater.java
@@ -0,0 +1,32 @@
+/**
+ * ### 1.1 티켓 판매 어플리케이션 구현하기
+ *
+ * - 소극장을 구현하는 클래스
+ * */
+public class Theater {
+
+    private TicketSeller ticketSeller;
+
+    public Theater(TicketSeller ticketSeller) {
+        this.ticketSeller = ticketSeller;
+    }
+
+    /**
+     * 관람객을 맞이함
+     *
+     * case1. 관람객의 가방에 초대권이 있는 경우 관람객의 amount 는 차감되지 않고 티켓 판매원에게 티켓을 받을 수 있음
+     * case2. 관람객의 초대권을 갖고 있지 않은 경우 관람객은 티켓 판매원을 통해 티켓을 구매할 수 있음
+     * */
+    public void enter(Audience audience) {
+        if (audience.getBag().hasInvitation()) {
+            Ticket ticket = ticketSeller.getTicketOffice().getTicket();
+            audience.getBag().setTicket(ticket);
+        } else {
+            Ticket ticket = ticketSeller.getTicketOffice().getTicket();
+            audience.getBag().minusAmount(ticket.getFee());
+            ticketSeller.getTicketOffice().plusAmount(ticket.getFee());
+            audience.getBag().setTicket(ticket);
+        }
+    }
+
+}
