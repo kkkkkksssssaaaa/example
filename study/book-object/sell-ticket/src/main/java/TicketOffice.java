680b6eb19a9becb3364716a81261a130e641ea3c
kkkkkksssssaaaa <kofc312@naver.com>
2022-04-18 22:37:03 +0900

    refactor: #1-3 modify method declare sell tickets

--- study/book-object/sell-ticket/src/main/java/TicketOffice.java
+++ study/book-object/sell-ticket/src/main/java/TicketOffice.java
@@ -3,7 +3,7 @@ import java.util.Arrays;
 import java.util.List;
 
 /**
- * ### 1.1 티켓 판매 어플리케이션 구현하기
+ * ### 1.3 설계 개선하기
  *
  * - 매표소를 구현하는 클래스
  * - 판매하거나 교환해 줄 티켓의 목록과 판매 금액을 필드로 가짐
@@ -18,15 +18,15 @@ public class TicketOffice {
         this.tickets.addAll(Arrays.asList(tickets));
     }
 
-    public Ticket getTicket() {
+    public void sellTicketTo(Audience audience) {
+        plusAmount(audience.buy(getTicket()));
+    }
+
+    private Ticket getTicket() {
         return tickets.remove(0);
     }
 
-    public void minusAmount(Long amount) {
-        this.amount -= amount;
-    }
-
-    public void plusAmount(Long amount) {
+    private void plusAmount(Long amount) {
         this.amount += amount;
     }
 
