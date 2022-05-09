25a0b72c57e3fc1191fec733d63205876d2f8794
kkkkkksssssaaaa <kofc312@naver.com>
2022-05-09 22:42:56 +0900

    feat: #2-3 modify class to abstract for DiscountPolicy

--- study/book-object/sell-ticket/src/main/java/screen/DiscountPolicy.java
+++ study/book-object/sell-ticket/src/main/java/screen/DiscountPolicy.java
@@ -1,15 +1,32 @@
 package screen;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * ### 2.3 할인 요금 구하기
  *
  * - `영화의 할인 정책` 을 구현하는 클래스
  * */
-public class DiscountPolicy {
+public abstract class DiscountPolicy {
 
-    // TODO 상영 정보를 통해 할인된 금액을 반환
-    public Money calculateDiscountAmount(Screening screening) {
-        return screening.getMovieFee();
+    private List<DiscountCondition> conditions = new ArrayList<>();
+
+    public DiscountPolicy(DiscountCondition... conditions) {
+        this.conditions = Arrays.asList(conditions);
     }
+
+    public Money calculateDiscountAmount(Screening screening) {
+        for (DiscountCondition each : conditions) {
+            if (each.isSatisfiedBy(screening)) {
+                return getDiscountAmount(screening);
+            }
+        }
+
+        return Money.ZERO;
+    }
+
+    abstract protected Money getDiscountAmount(Screening screening);
 
 }
