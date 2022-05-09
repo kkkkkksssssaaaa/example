e4a9f43457c97fcc29581623e832f59a1fb064c0
kkkkkksssssaaaa <kofc312@naver.com>
2022-05-09 22:29:53 +0900

    feat: #2-3 modify movie class field

--- study/book-object/sell-ticket/src/main/java/screen/Movie.java
+++ study/book-object/sell-ticket/src/main/java/screen/Movie.java
@@ -1,16 +1,31 @@
 package screen;
 
+import java.time.Duration;
+
 /**
- * ### 2.2 객체지향 프로그래밍을 향해
+ * ### 2.3 할인 요금 구하기
  *
  * - `영화` 를 구현하는 클래스
  * */
 public class Movie {
 
-    private Money money;
+    private String title;
+    private Duration runningTime;
+    private Money fee;
+    private DiscountPolicy discountPolicy;
+
+    public Movie(String title,
+                 Duration runningTime,
+                 Money fee,
+                 DiscountPolicy discountPolicy) {
+        this.title = title;
+        this.runningTime = runningTime;
+        this.fee = fee;
+        this.discountPolicy = discountPolicy;
+    }
 
     public Money getFee() {
-        return this.money;
+        return this.fee;
     }
 
     /**
@@ -19,7 +34,7 @@ public class Movie {
      * - 영화의 예매 금액 계산
      * */
     public Money calculateMovieFee(Screening screening) {
-        return this.money;
+        return this.fee;
     }
 
 }
