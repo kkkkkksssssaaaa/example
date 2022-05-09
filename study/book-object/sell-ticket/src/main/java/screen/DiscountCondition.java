3c701f424a9d2caa61fbefd0bcc3593ff66fcc41
kkkkkksssssaaaa <kofc312@naver.com>
2022-05-09 22:41:48 +0900

    feat: #2-3 add DiscountCondition

---
+++ study/book-object/sell-ticket/src/main/java/screen/DiscountCondition.java
@@ -0,0 +1,15 @@
+package screen;
+
+/**
+ * ### 2.3 할인 요금 구하기
+ *
+ * - `영화의 할인 조건` 을 구현하는 클래스
+ * */
+public class DiscountCondition {
+
+    // TODO 영화 정보를 통해 할인 조건에 부합하는지 여부 판단하여 반환
+    public boolean isSatisfiedBy(Screening screening) {
+        return true;
+    }
+
+}
