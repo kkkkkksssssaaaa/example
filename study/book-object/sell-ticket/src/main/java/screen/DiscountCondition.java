2b75fed772e6ae944841f23c797b1f6e8798b019
kkkkkksssssaaaa <kofc312@naver.com>
2022-05-29 21:12:38 +0900

    refactor: #2-3 change class to interface

--- study/book-object/sell-ticket/src/main/java/screen/DiscountCondition.java
+++ study/book-object/sell-ticket/src/main/java/screen/DiscountCondition.java
@@ -5,11 +5,9 @@ package screen;
  *
  * - `영화의 할인 조건` 을 구현하는 클래스
  * */
-public class DiscountCondition {
+public interface DiscountCondition {
 
     // TODO 영화 정보를 통해 할인 조건에 부합하는지 여부 판단하여 반환
-    public boolean isSatisfiedBy(Screening screening) {
-        return true;
-    }
+    boolean isSatisfiedBy(Screening screening);
 
 }
