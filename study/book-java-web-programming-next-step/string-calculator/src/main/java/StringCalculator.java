ccb6e2635f1eadb0f4f839c56c96906de3619585
kkkkkksssssaaaa <kofc312@naver.com>
2022-06-01 21:21:10 +0900

    refactor: #2-4 sum 메서드 분리

--- study/book-java-web-programming-next-step/string-calculator/src/main/java/StringCalculator.java
+++ study/book-java-web-programming-next-step/string-calculator/src/main/java/StringCalculator.java
@@ -6,6 +6,11 @@ public class StringCalculator {
         }
 
         String[] values = text.split(",");
+
+        return sum(values);
+    }
+
+    private int sum(String[] values) {
         int sum = 0;
 
         for (String value : values) {
