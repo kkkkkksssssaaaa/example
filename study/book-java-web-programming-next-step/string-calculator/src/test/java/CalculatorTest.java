d9e2032928a76321350b4e63af3ee34f245ad91a
kkkkkksssssaaaa <kofc312@naver.com>
2022-06-01 20:46:16 +0900

    refactor: #2-2 테스트 어노테이션을 사용하도록 변경

--- study/book-java-web-programming-next-step/string-calculator/src/test/java/CalculatorTest.java
+++ study/book-java-web-programming-next-step/string-calculator/src/test/java/CalculatorTest.java
@@ -1,29 +1,30 @@
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class CalculatorTest {
 
-    public static void main(String[] args) {
+    @Test
+    public void add() {
         Calculator cal = new Calculator();
-
-        add(cal);
-        subtract(cal);
-        multiply(cal);
-        divide(cal);
-    }
-
-    private static void add(Calculator cal) {
         System.out.println(cal.add(3, 4));
     }
 
-    private static void subtract(Calculator cal) {
+    @Test
+    public void subtract() {
+        Calculator cal = new Calculator();
         System.out.println(cal.subtract(5, 4));
     }
 
-    private static void multiply(Calculator cal) {
+    @Test
+    public void multiply() {
+        Calculator cal = new Calculator();
         System.out.println(cal.multiply(2, 6));
     }
 
-    private static void divide(Calculator cal) {
+    @Test
+    public void divide() {
+        Calculator cal = new Calculator();
         System.out.println(cal.divide(8, 4));
     }
 
