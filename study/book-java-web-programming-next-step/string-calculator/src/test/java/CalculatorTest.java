a87a0d34df4c60d760a52d9cc4c028a731e3cdb4
kkkkkksssssaaaa <kofc312@naver.com>
2022-06-01 20:49:05 +0900

    refactor: #2-2 콘솔 출력 대신 assertion api 를 사용하도록 변경

--- study/book-java-web-programming-next-step/string-calculator/src/test/java/CalculatorTest.java
+++ study/book-java-web-programming-next-step/string-calculator/src/test/java/CalculatorTest.java
@@ -7,25 +7,29 @@ class CalculatorTest {
     @Test
     public void add() {
         Calculator cal = new Calculator();
-        System.out.println(cal.add(3, 4));
+
+        assertEquals(9, cal.add(6, 3));
     }
 
     @Test
     public void subtract() {
         Calculator cal = new Calculator();
-        System.out.println(cal.subtract(5, 4));
+
+        assertEquals(3, cal.subtract(6, 3));
     }
 
     @Test
     public void multiply() {
         Calculator cal = new Calculator();
-        System.out.println(cal.multiply(2, 6));
+
+        assertEquals(12, cal.multiply(2, 6));
     }
 
     @Test
     public void divide() {
         Calculator cal = new Calculator();
-        System.out.println(cal.divide(8, 4));
+
+        assertEquals(2, cal.divide(8, 4));
     }
 
 }
