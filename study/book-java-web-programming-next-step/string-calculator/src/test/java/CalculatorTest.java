abcf3f2f6d2c9c201493aaa226d0a7db82b75d1c
kkkkkksssssaaaa <kofc312@naver.com>
2022-06-01 20:43:26 +0900

    refactor: #2-1 계산기 테스트 클래스 메서드 분리

--- study/book-java-web-programming-next-step/string-calculator/src/test/java/CalculatorTest.java
+++ study/book-java-web-programming-next-step/string-calculator/src/test/java/CalculatorTest.java
@@ -5,9 +5,25 @@ class CalculatorTest {
     public static void main(String[] args) {
         Calculator cal = new Calculator();
 
+        add(cal);
+        subtract(cal);
+        multiply(cal);
+        divide(cal);
+    }
+
+    private static void add(Calculator cal) {
         System.out.println(cal.add(3, 4));
+    }
+
+    private static void subtract(Calculator cal) {
         System.out.println(cal.subtract(5, 4));
+    }
+
+    private static void multiply(Calculator cal) {
         System.out.println(cal.multiply(2, 6));
+    }
+
+    private static void divide(Calculator cal) {
         System.out.println(cal.divide(8, 4));
     }
 
