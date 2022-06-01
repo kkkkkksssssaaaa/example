6174637df4de8fda068fc45442be4a9992598801
kkkkkksssssaaaa <kofc312@naver.com>
2022-06-01 20:51:00 +0900

    refactor: #2-2 계산기 인스턴스 생성 중복 코드 제거

--- study/book-java-web-programming-next-step/string-calculator/src/test/java/CalculatorTest.java
+++ study/book-java-web-programming-next-step/string-calculator/src/test/java/CalculatorTest.java
@@ -1,34 +1,34 @@
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class CalculatorTest {
 
+    private Calculator cal = new Calculator();
+
+    @BeforeEach
+    public void setup() {
+        cal = new Calculator();
+    }
+
     @Test
     public void add() {
-        Calculator cal = new Calculator();
-
         assertEquals(9, cal.add(6, 3));
     }
 
     @Test
     public void subtract() {
-        Calculator cal = new Calculator();
-
         assertEquals(3, cal.subtract(6, 3));
     }
 
     @Test
     public void multiply() {
-        Calculator cal = new Calculator();
-
         assertEquals(12, cal.multiply(2, 6));
     }
 
     @Test
     public void divide() {
-        Calculator cal = new Calculator();
-
         assertEquals(2, cal.divide(8, 4));
     }
 
